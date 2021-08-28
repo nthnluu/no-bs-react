7bc267fe594f95efca85c4c65dffee876523eb56
nathanluu <nate1299@me.com>
2021-08-28 11:44:06 -0400

    AccountMenu shared component | Refactored tests to be more extensible.

--- src/components/shared/AccountMenu/AccountMenu.test.tsx
+++ src/components/shared/AccountMenu/AccountMenu.test.tsx
@@ -14,26 +14,34 @@ describe('account menu', () => {
             .toBe("/sample_avatar.jpg");
     });
 
-    it('should open the account menu when clicked', async () => {
+    it('should display the menu when the account button is clicked', async () => {
+        const expectedElements: {
+            type: "text" | "button";
+            text: string;
+        }[] = [
+            {type: "text", text: testUser().name},
+            {type: "text", text: testUser().email},
+            {type: "button", text: "Sign out"},
+            {type: "button", text: "Manage account"}];
+
         render(<AccountMenu user={testUser()}/>);
-        expect(screen.queryByText(testUser().name)).toBeNull();
-        expect(screen.queryByText(testUser().email)).toBeNull();
-        expect(screen.queryByText("Sign out")).toBeNull();
-        expect(screen.queryByText("Manage account")).toBeNull();
 
-        const menuButton = screen.getByRole("button");
-        menuButton.click();
+        // Check that the expected items are hidden when the menu is closed
+        expectedElements.forEach(elem => expect(screen.queryByText(elem.text)).toBeNull());
 
+        // Open the menu
+        screen.getByRole("button").click();
         await waitFor(() => screen.queryByText(testUser().name));
-        expect(screen.queryByText(testUser().name)).toBeInTheDocument();
-        expect(screen.queryByText(testUser().email)).toBeInTheDocument();
 
-        const signOutButton = screen.queryByText("Sign out");
-        expect(signOutButton).toBeInTheDocument();
-        expect(signOutButton).toBeEnabled();
-
-        const manageAccountButton = screen.queryByText("Manage account");
-        expect(manageAccountButton).toBeInTheDocument();
-        expect(manageAccountButton).toBeEnabled();
+        // Check that the expected items displayed when the menu is open
+        expectedElements.forEach(elem => {
+            if (elem.type === "text") {
+                expect(screen.queryByText(elem.text)).toBeInTheDocument();
+            } else if (elem.type === "button") {
+                const button = screen.queryByText(elem.text);
+                expect(button).toBeInTheDocument();
+                expect(button).toBeEnabled();
+            }
+        });
     });
 });
